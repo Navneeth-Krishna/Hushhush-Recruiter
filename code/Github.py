edf6258ee08dc5665bed89756e16b74d58728da0
Navneeth-Krishna <a.navneethkrishna@gmail.com>
2024-09-16 21:47:57 +0200

    Added try except block

--- code/Github.py
+++ code/Github.py
@@ -46,36 +46,40 @@ headers = {
 }
 for j in range (0,1000,30):
    link = f"https://api.github.com/users?since={j}"
-   response = requests.get(link, headers=headers, timeout=5)
-   response.raise_for_status()
-   results = response.json()
-   for i in range(len(results)):
-    Git_users_dict = {}
-    data=(results[i])
-   #  To avoid duplicate fetching of same user data
-    if(data['id'] > completed_user_id):  
-      usr = requests.get(results[i]['url']).json()
-      user_url.append(usr)
-      repos_url = requests.get(data['repos_url'], headers= headers, timeout=5)
-      repos_url.raise_for_status
-      reposdata = repos_url.json()
-      starred_url = data['starred_url'].split('{')[0]
-      starred_repo = requests.get(starred_url, headers= headers, timeout=5)
-      starred_repo.raise_for_status()
-      stareddata = starred_repo.json()
-      
-      # Get the basic user details and get the repo url
-      if(user_url[i]['name']):
-         print(f"Getting data for user id:{data['id']}")
-         Git_users_dict = userdetails(data['id'],user_url[i])
-         User_Data.append(Git_users_dict)
-         time.sleep(1)
+   try:
+      response = requests.get(link, headers=headers, timeout=5)
+      response.raise_for_status()
+      results = response.json()
+      for i in range(len(results)):
+         Git_users_dict = {}
+         data=(results[i])
+         #  To avoid duplicate fetching of same user data
+         if(data['id'] > completed_user_id):  
+            usr = requests.get(results[i]['url']).json()
+            user_url.append(usr)
+            repos_url = requests.get(data['repos_url'], headers= headers, timeout=5)
+            repos_url.raise_for_status
+            reposdata = repos_url.json()
+            starred_url = data['starred_url'].split('{')[0]
+            starred_repo = requests.get(starred_url, headers= headers, timeout=5)
+            starred_repo.raise_for_status()
+            stareddata = starred_repo.json()
+            
+         # Get the basic user details and get the repo url
+         if(user_url[i]['name']):
+            print(f"Getting data for user id:{data['id']}")
+            Git_users_dict = userdetails(data['id'],user_url[i])
+            User_Data.append(Git_users_dict)
+            time.sleep(1)
 
-         # Gets the Repo data of user
-         Repo_dict = UserRepoDetails(data['id'], reposdata)
-         Repos_Data.extend(Repo_dict)
-         time.sleep(1)
-   completed_user_id = data['id']
+            # Gets the Repo data of user
+            Repo_dict = UserRepoDetails(data['id'], reposdata)
+            Repos_Data.extend(Repo_dict)
+            time.sleep(1)
+      completed_user_id = data['id']
+
+   except Exception as e:
+      print(f"failed to get data of user{j} with status code{e}")
 
 
 users_df = pd.DataFrame(User_Data)
